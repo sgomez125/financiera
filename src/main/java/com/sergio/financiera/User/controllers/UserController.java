dcd38b0445fa350df323c8ec512fe98c4cff1704
Sergio Gómez <sgomez@digitalgeko.com>
2018-11-05 16:46:43 -0600

    add user

--- src/main/java/com/sergio/financiera/User/controllers/UserController.java
+++ src/main/java/com/sergio/financiera/User/controllers/UserController.java
@@ -3,10 +3,13 @@ package com.sergio.financiera.User.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import com.sergio.financiera.User.models.entities.User;
 import com.sergio.financiera.User.services.UserService;
@@ -21,5 +24,13 @@ public class UserController {
 	@ResponseBody
 	public List<User> findAll() {
 		return userService.listUsers();
+	}
+	
+	@RequestMapping(value="/add", method = RequestMethod.POST)
+	@ResponseStatus(HttpStatus.OK)
+	public void addUser(@RequestParam("username")String username){
+		User user = new User();
+		user.setUserName(username);
+		userService.saveUser(user);
 	}
 }
